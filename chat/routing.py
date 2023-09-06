f625358db7b3a6b1c28035320bb4bdd1f6adbd25
Chetan <chetan.aiofficial@gmail.com>
2023-09-06 21:35:17 +0530

    deleted chat_app

--- chat/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
-from chat import consumers
-
-application = ProtocolTypeRouter({
-    "websocket": URLRouter([
-        path("ws/chat/", consumers.ChatConsumer.as_asgi()),
-    ]),
-})
--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls', namespace='user_list')),
     path('', include('content.urls', namespace='content_list')),
-    path('', include('chat.urls', namespace='load_chat')),
     path('', include('analytics.urls', namespace='Analytics')),
     path('', include('payment.urls', namespace='payment_done'))
 ]
