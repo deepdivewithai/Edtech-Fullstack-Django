c4cde1398d90a29faa963782080c2be8128d1a88
Chetan <chetan.aiofficial@gmail.com>
2023-09-06 22:28:46 +0530

    added media root in core/urls.py

--- content/urls.py
+++ content/urls.py
@@ -6,5 +6,4 @@ app_name = 'content_list'
 urlpatterns = [
     path('content/', ContentListCreateView.as_view(), name='content-list-create'),
     path('content/<int:pk>/', ContentDetailView.as_view(), name='content-detail'),
-    # Other URL patterns for content-related views and actions.
 ]
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('', include('analytics.urls', namespace='Analytics')),
     path('', include('payment.urls', namespace='payment_done'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
