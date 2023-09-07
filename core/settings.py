e4e25c625ba684d9153a6e745d8adc5fd2ab6a55
Chetan <chetan.aiofficial@gmail.com>
2023-09-07 18:11:21 +0530

    added postgres

--- core/settings.py
+++ core/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "core_one",      # Database name is "core"
+        "USER": "postgres",  # Database user is "postgres"
+        "PASSWORD": "postgres",  # Password is "postgres"
+        "HOST": "localhost",  # Database host is "localhost"
+        "PORT": "5432",       # Database port is "5432" (default)
     }
 }
 
